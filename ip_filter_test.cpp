156783e4f8bf2bf857e816253c61ad92952b8cc6
lex-96 <lex9612@gmail.com>
2018-03-18 00:20:11 +0300

    main

--- ip_filter_test.cpp
+++ ip_filter_test.cpp
@@ -7,12 +7,12 @@ BOOST_AUTO_TEST_SUITE(test_suite_main)
 
 BOOST_AUTO_TEST_CASE(test_split) {
     BOOST_CHECK( split( "aaaa    aaa    aa    a", '    ') == std::vector<std::string>({ "aaaa", "aaa", "aa", "a"}) );
-    BOOST_CHECK( split(".." ,"." ) == std::vector<std::string>({"", "", "" }) );
-    BOOST_CHECK( split("aa.aa." ,"." ) == std::vector<std::string>({"aa", "aa", "" }) );
-    BOOST_CHECK( split(".aa.aa" ,"." ) == std::vector<std::string>({"", "aa", "aa" }) );
-    BOOST_CHECK( split("aa..aa" ,"." ) == std::vector<std::string>({"aa", "", "aa" }) );
-    BOOST_CHECK( split("aa" ,"." ) == std::vector<std::string>({"aa" }) );
-    BOOST_CHECK( split("" ,"." ) == std::vector<std::string>({"" }) );
+    BOOST_CHECK( split(".." , '.') == std::vector<std::string>({"", "", "" }) );
+    BOOST_CHECK( split("aa.aa." , '.') == std::vector<std::string>({"aa", "aa", "" }) );
+    BOOST_CHECK( split(".aa.aa" , '.') == std::vector<std::string>({"", "aa", "aa" }) );
+    BOOST_CHECK( split("aa..aa" , '.') == std::vector<std::string>({"aa", "", "aa" }) );
+    BOOST_CHECK( split("aa" , '.') == std::vector<std::string>({"aa" }) );
+    BOOST_CHECK( split("" , '.') == std::vector<std::string>({"" }) );
 }
 
 BOOST_AUTO_TEST_CASE(test_getIP) {
